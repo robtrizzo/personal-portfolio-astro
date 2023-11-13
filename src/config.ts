9a14d79775970b3bc760e8b2d5f04eabb8ef6bf8
Robert Rizzo <robert@broadtime.com>
2023-11-13 09:10:49 -0500

    first commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Robert Rizzo | Personal Portfolio Website';
+export const SITE_DESCRIPTION =
+  'This is a Personal Portfolio Website built with Astro and TailwindCSS from the Astrofy template.';
